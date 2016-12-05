a0af00c0d4ddcdc9e2be07d038215fb1909edc05
Mauya <smkadungure@live.co.uk>
2016-12-05 06:11:25 +0000

    changing the home file and about file

--- js/controller.js
+++ js/controller.js
@@ -4,17 +4,17 @@ angular.module('RouteControllers', [])
         $scope.myInterval = 5000;
         $scope.slides= [
           {
-              image: "images/swing1.jpg"
+            image: "images/swing1.jpg"
           },
           {
-              image: "images/swing2.jpg"
+            image: "images/swing2.jpg"
           },
           {
-              image: "images/swing3.jpg"
+            image: "images/swing3.jpg"
           },
           {
-              image: "images/swing4.jpg"
-          },
+            image: "images/swing4.jpg"
+          }
         ];
       })
     .controller('navController', function($scope,$location){
@@ -27,29 +27,30 @@ angular.module('RouteControllers', [])
     $scope.members=[
       {
       "name":"member one",
-      "image":"images/member1.jpg"
+      "img":"images/member1.jpg"
       },
 
       {
         "name":"member Two",
-        "image":"images/member2.jpg"
+        "img":"images/member2.jpg"
       },
 
       {
         "name":"member Three",
-        "image":"images/member3.jpg"
+        "img":"images/member3.jpg"
       },
       
       {
         "name":"member Four",
-        "image":"images/member4.jpg"
+        "img":"images/member4.jpg"
       },
 
       {
         "name":"member Five",
-        "image":"images/member5.jpg"
+        "img":"images/member5.jpg"
       },  
-    ]
+    ];
+
     $scope.subscribe = {
     name: null
   }
@@ -86,7 +87,50 @@ var media = [
 }]
   $scope.media=media;
 })
-.controller('MusicController', function ($scope) .....)
+.controller('MusicController', function ($scope){
+    return {
+      restrict: 'E',
+      scope: {},
+      controller: function($scope, $element) {
+        $scope.audio = new Audio();
+        $scope.currentNum = 0;
+
+        // tell others to give me my prev/next track (with audio.set message)
+        $scope.next = function(){ $rootScope.$broadcast('audio.next'); };
+        $scope.prev = function(){ $rootScope.$broadcast('audio.prev'); };
+
+        // tell audio element to play/pause, you can also use $scope.audio.play() or $scope.audio.pause();
+        $scope.playpause = function(){ var a = $scope.audio.paused ? $scope.audio.play() : $scope.audio.pause(); };
+
+        // listen for audio-element events, and broadcast stuff
+        $scope.audio.addEventListener('play', function(){ $rootScope.$broadcast('audio.play', this); });
+        $scope.audio.addEventListener('pause', function(){ $rootScope.$broadcast('audio.pause', this); });
+        $scope.audio.addEventListener('timeupdate', function(){ $rootScope.$broadcast('audio.time', this); });
+        $scope.audio.addEventListener('ended', function(){ $rootScope.$broadcast('audio.ended', this); $scope.next(); });
+
+        // set track & play it
+        $rootScope.$on('audio.set', function(r, file, info, currentNum, totalNum){
+          var playing = !$scope.audio.paused;
+          $scope.audio.src = file;
+          var a = playing ? $scope.audio.play() : $scope.audio.pause();
+          $scope.info = info;
+          $scope.currentNum = currentNum;
+          $scope.totalNum = totalNum;
+        });
+
+        // update display of things - makes time-scrub work
+        setInterval(function(){ $scope.$apply(); }, 500);
+      },      
+    };
+  })
+
+.controller('VideoController', function ($scope){
+
+})
+
+.controller('PhotoController', function ($scope){
+  
+})
 
 
 .controller("ContactController", function(){
